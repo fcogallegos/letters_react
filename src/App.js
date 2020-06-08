e78eb35236440f6472f5c18934482f8aa41ae110
francisco28 <fgallegosgo@gmail.com>
2020-06-08 14:03:38 -0600

    get information of artist since other api

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
   const [ searchLetter, saveSearchLetter ] = useState({});
   //state to lyrics
   const [ lyrics, saveLyrics ] = useState('');
+  //state ti info
+  const [ info, saveInfo ] = useState({});
 
   useEffect(() => {
       //review if a object is empty
@@ -18,10 +20,18 @@ function App() {
       const consultApiLyrics = async () => {
           
           const { artist, song } = searchLetter;   
-          const url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+          const url_lyrics = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+          const url_info = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
 
-          const result = await axios(url);
-          saveLyrics(result.data.lyrics);
+          const [ lyrics, information ] = await Promise.all([
+            axios(url_lyrics),
+            axios(url_info)
+          ]);
+          
+          saveLyrics(lyrics.data.lyrics);
+          saveInfo(information.data.artists[0]);
+          
+          //saveLyrics(result.data.lyrics);
       } 
       consultApiLyrics();
   }, [searchLetter]);
