f8acc4379f02534e0024571e1eee63981c80be9f
francisco28 <fgallegosgo@gmail.com>
2020-06-08 11:45:10 -0600

    add the Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,48 @@ import React from 'react';
 
 const Form = () => {
     return ( 
-        <h1>Form</h1>
+        <div className="bg-info">
+            <div className="container">
+                <div className="row">
+                    <form 
+                        className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+                    >
+                        <fieldset>
+                            <legend className="text-center">Song yrics finder</legend>
+
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Artist</label>
+                                        <input 
+                                            type="text"
+                                            className="form-control"
+                                            name="artist"
+                                            placeholder="Artist Name"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label>Song</label>
+                                        <input 
+                                            type="text"
+                                            className="form-control"
+                                            name="song"
+                                            placeholder="Song Name"
+                                        />
+                                    </div>
+                                </div>                                    
+                            </div>
+                            <button 
+                                type="submit"
+                                className="btn btn-primary float-right"
+                            >Search</button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
      );
 }
  
